cff4da1e2c73456d8ef9a6971098561fdb761bcd
ozer619 <prathameshd419@gmail.com>
2021-03-20 19:15:36 +0530

    cleaning junk files

--- initialize.sh
+++ initialize.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #This is the initial file which should be executed so that the base folder for 
-<<<<<<< HEAD
-#storing is created where the templates will be stored
-
+#storing templates is created 
 if [[ -d "$HOME/.config/Autotemp/templates" ]]
 then
    echo "$HOME/.config/Autotemp/templates already exist's"
 else
     mkdir -p $HOME/.config/Autotemp/templates
 fi
-=======
-#storing templates is created 
->>>>>>> ea455ebac030d41d4d95884d047c015a0f204b72
